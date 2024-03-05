a7475bcd1f60b2cdc6d69891fc056391489994c9
Hamdiya141 <hamdimahad141@gmail.com>
2024-03-05 14:46:19 +0300

    Done

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function name(params) {
+    return 2000/100
+}
+
+function square(parameter) {
+    return parameter * parameter
+}
+
+ function add(parameter1, parameter2) {
+    return parameter1 + parameter2
+ }   
